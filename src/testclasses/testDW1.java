15e57a472d5a4da6177b9137720ece7f2902cfa6
francescogarg <67015406+francescogarg@users.noreply.github.com>
2020-07-30 23:40:40 +0200

    prova 9 lancio jar su travis

---
+++ src/testclasses/testDW1.java
@@ -0,0 +1,23 @@
+package testclasses;
+
+public class testDW1 {
+    public void foo() {
+        PowerManager.WakeLock wakeLock1 = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK | PowerManager.ON_AFTER_RELEASE, "wakelock1");
+        wakeLock1.acquire();
+
+        PowerManager.WakeLock wakeLock2 = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK | PowerManager.ON_AFTER_RELEASE, "wakelock2");
+        wakeLock2.acquire();
+    }
+
+    public void foo2() {
+        PowerManager.WakeLock wakeLock3 = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK | PowerManager.ON_AFTER_RELEASE, "wakelock3");
+        wakeLock3.acquire();
+        wakeLock3.release();
+    }
+
+    public void foo3() {
+        PowerManager.WakeLock wakeLock4 = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK | PowerManager.ON_AFTER_RELEASE, "wakelock4");
+        wakeLock4.acquire();
+    }
+
+}
---
+++ src/testclasses/testNoSmell.java
@@ -0,0 +1,11 @@
+package testclasses;
+
+public class testNoSmell {
+
+    public void foo() {
+        PowerManager.WakeLock wakeLock4 = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK | PowerManager.ON_AFTER_RELEASE, "wakelock4");
+        wakeLock4.acquire();
+        wakeLock4.release();
+    }
+
+}
