511004de373873cfbcaf7a9c6d36bc5227510c06
francescogarg <67015406+francescogarg@users.noreply.github.com>
2020-08-11 12:48:15 +0200

    reset

--- src/testclasses/testDW1.java
+++ src/testclasses/testDW1.java
@@ -1,25 +1,22 @@
 package testPackage;
 
 public class testDW1 {
-    public static void foo() {
+    public void foo() {
         PowerManager.WakeLock wakeLock1 = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK | PowerManager.ON_AFTER_RELEASE, "wakelock1");
         wakeLock1.acquire();
 
         PowerManager.WakeLock wakeLock2 = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK | PowerManager.ON_AFTER_RELEASE, "wakelock2");
         wakeLock2.acquire();
-		wakeLock1.release();
-		wakeLock2.relea static se();
     }
 
-    public static void foo2() {
+    public void foo2() {
         PowerManager.WakeLock wakeLock3 = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK | PowerManager.ON_AFTER_RELEASE, "wakelock3");
         wakeLock3.acquire();
         wakeLock3.release();
     }
 
-    public static void foo3() {
+    public void foo3() {
         PowerManager.WakeLock wakeLock4 = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK | PowerManager.ON_AFTER_RELEASE, "wakelock4");
         wakeLock4.acquire();
-		wakeLock4.release();
     }
 }
--- src/testclasses/testDW_ERB1.java
+++ src/testclasses/testDW_ERB1.java
@@ -10,34 +10,29 @@ public class testDW_ERB1 {
         lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         if (lm.getAllProviders().contains(LocationManager.GPS_PROVIDER)) {
             if (lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, (LocationListener) this);
             }
         }
 
         lm2 = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         if (lm2.getAllProviders().contains(LocationManager.GPS_PROVIDER)) {
             if (lm2.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                lm2.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, (LocationListener) this);
             }
         }
     }
 
-	public void onResume() {
-		lm2.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, (LocationListener) this);
-		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, (LocationListener) this) static ;
-	}
-
-    public static void foo() {
- static        PowerManager.WakeLock wakeLock1 = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK | PowerManager.ON_AFTER_RELEASE, "wakelock1");
+    public void foo() {
+        PowerManager.WakeLock wakeLock1 = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK | PowerManager.ON_AFTER_RELEASE, "wakelock1");
         wakeLock1.acquire();
-		wakeLock1.release();
     }
 
-    public static void foo2() {
+    public void foo2() {
         PowerManager.WakeLock wakeLock2 = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK | PowerManager.ON_AFTER_RELEASE, "wakelock2");
         wakeLock2.acquire();
-		wakeLock2.release();
     }
 
-    public static void foo3() {
+    public void foo3() {
         PowerManager.WakeLock wakeLock3 = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK | PowerManager.ON_AFTER_RELEASE, "wakelock3");
         wakeLock3.acquire();
         wakeLock3.release();
